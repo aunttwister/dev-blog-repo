af6183836d8ca4e9b18d7b43c6d76a5f283f6433
Pavle Curcic <developer.sedna@outlook.com>
2022-07-21 22:48:37 +0200

    updating

--- src/dev-blog-api/Models/File.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace dev_blog_api.Models
-{
-    public class File
-    {
-        public string FileName { get; set; }
-        public DateTime PublishDate { get; set; }
-        public string FullName => $"{PublishDate:yyyy-MM-dd}#{FileName}.md";
-    }
-}
--- src/dev-blog-api/Services/IFileService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace dev_blog_api.Services
-{
-    public interface IFileService
-    {
-        public List<Models.File> GetFilesInfo(string type);
-    }
-}
