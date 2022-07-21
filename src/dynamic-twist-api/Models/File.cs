6ed5cb7910eb20dca1234c9e42df0fbbf5a9be5a
Pavle Curcic <developer.sedna@outlook.com>
2022-07-21 22:49:04 +0200

    updating 2

---
+++ src/dynamic-twist-api/Models/File.cs
@@ -0,0 +1,9 @@
+﻿namespace dev_blog_api.Models
+{
+    public class File
+    {
+        public string FileName { get; set; }
+        public DateTime PublishDate { get; set; }
+        public string FullName => $"{PublishDate:yyyy-MM-dd}#{FileName}.md";
+    }
+}
